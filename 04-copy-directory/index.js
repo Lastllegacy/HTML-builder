a9392e47114ec5194fd5a412c1732b08c6171c0d
Nikita Kalyakin <forrkko@gmail.com>
2022-11-07 14:08:18 +0700

    feat: complete task 04-copy-folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require("fs")
+const path = require("node:path")
+
+const filesDir = (file = '.') => {
+   return path.resolve(__dirname, 'files', file)
+} 
+
+const copyFilesDir = (file = '.') => {
+   return path.resolve(__dirname, "files-copy" , file)
+} 
+
+fs.mkdir(copyFilesDir(), {recursive:true}, err => {
+   if(err) throw err
+   
+   fs.readdir(filesDir(), (err,files) => {
+      if(err) throw err;
+
+      files.forEach(file => {
+         fs.copyFile(filesDir(file), copyFilesDir(file), (err) => {
+            if(err) throw err
+         })
+      })
+   })
+})
+
 
