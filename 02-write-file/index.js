368c76f4373d57ff54402dfb8c1709df86aeb4b5
Nikita Kalyakin <forrkko@gmail.com>
2022-11-07 11:55:58 +0700

    fix: remove useless comments

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("node:path");
 const { stdin: input, stdout: output } = require("node:process");
 const readline = require('node:readline');
-// const { stdin } = require("process");
 
 const rl = readline.createInterface({input, output});
 
