2813496bdcb28d6ce733d028c6d3fefb850b8a22
Lastllegacy <forrkko@gmail.com>
2022-11-10 14:23:54 +0700

    fix: refactor 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,19 +1,12 @@
 const fs = require('fs');
-const path = require('node:path')
+const path = require('node:path');
+const { stdout } = require('process');
 
 const txtDir = path.join(__dirname, '/text.txt')
 
-const stream = new fs.ReadStream(txtDir, {encoding:'utf-8'});
+const reading  = fs.createReadStream(txtDir, 'utf-8')
 
-fs.readFile(txtDir, (err,data) => {
-   if(err) throw err;
-   console.log(data.toString());
-})
+reading.pipe(stdout);
 
-stream.on('error' , (err) => {
-   if(err.code == 'ENOENT') console.log('Файл не найден!')
-   else {
-      console.error(err)
-   }
-})
+
 
